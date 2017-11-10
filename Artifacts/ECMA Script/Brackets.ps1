a49468f1ce2ece81271bb119509375e953de6efc
garima20anand <garima20anand@outlook.com>
2017-11-10 17:32:15 +0530

    gh

---
+++ Artifacts/ECMA Script/Brackets.ps1
@@ -0,0 +1,4 @@
+﻿$uri = "https://aproloydevtestlab901.blob.core.windows.net/artifacts/Brackets.Release.1.11.msi"
+$out = "c:\Release.1.11.msi"
+Invoke-WebRequest -uri $uri -OutFile $out
+Start-Process -FilePath "msiexec.exe" -ArgumentList "/i $out /quiet /norestart /l c:\installlog.txt"
